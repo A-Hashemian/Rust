37bdb02a24d34ecc13e783a7a9bde08fd4f87d32
A-Hashemian <amin@hashemian.tech>
2022-09-23 23:53:18 +0300

    set lib

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
+use std::str::FromStr;
+use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+const MAX: u16 = 65535;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
