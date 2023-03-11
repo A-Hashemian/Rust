a30fac4187c99967b6f547c9336304a15d95983e
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-11 15:07:25 -0500

    Update simple_blockchain.rs

--- Blockchain/simple_blockchain.rs
+++ Blockchain/simple_blockchain.rs
@@ -1,1 +1,4 @@
 
+use std::fmt::{Display, Formatter, Result};
+// i am specifying that i'll be using the 'fmt' module from Rust's standard library.
+// This module contains useful functions, particularly for printing to the console.
