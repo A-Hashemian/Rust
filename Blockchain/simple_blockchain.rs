df8f73493b7a59466bf653216ed709d65a2d75bd
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-11 15:09:29 -0500

    Update simple_blockchain.rs

--- Blockchain/simple_blockchain.rs
+++ Blockchain/simple_blockchain.rs
@@ -2,3 +2,12 @@
 use std::fmt::{Display, Formatter, Result};
 // i am specifying that i'll be using the 'fmt' module from Rust's standard library.
 // This module contains useful functions, particularly for printing to the console.
+
+struct Block {
+    index: u32,
+    timestamp: u128,
+    data: String,
+    previous_hash: String,
+    hash: String,
+}
+// i define a struct named 'Block', which encapsulates the properties of a block (index, timestamp, data, previous_hash, hash).
