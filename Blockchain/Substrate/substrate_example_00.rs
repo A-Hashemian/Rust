a2cb534f4aa699756622b686ed950eb37d7f6293
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-23 16:16:16 -0400

    Update substrate_example_00.rs

--- Blockchain/Substrate/substrate_example_00.rs
+++ Blockchain/Substrate/substrate_example_00.rs
@@ -2,3 +2,10 @@
 use substrate_subxt::{balances::{Balances, AccountData}, system::System};
 use subxt::{Client, ClientBuilder, PairSigner};
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a new Substrate client
+    let client = ClientBuilder::<PairSigner>::new()
+        .set_url("wss://rpc.polkadot.io")
+        .build()
+        .await?;
