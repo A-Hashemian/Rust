68353a44031cb3d3fadc65f3f220095d8154ca17
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-01 17:14:28 +0300

    Update tcp_server_00.rs

--- Web/tcp_server_00.rs
+++ Web/tcp_server_00.rs
@@ -3,3 +3,10 @@ use std::net::{TcpListener, TcpStream};
 fn handle_client(stream: TcpStream) {
  // do something
 }
+
+fn main() -> std::io::Result<()> {
+ let listener = TcpListener::bind("127.0.0.1:80")?;
+ 
+ 
+ 
+}
