3a6782921109ca7bef993aaf1fafb28db034e490
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-01 09:48:25 -0400

    Update http_server_00.rs

--- Web/http_server_00.rs
+++ Web/http_server_00.rs
@@ -1,6 +1,8 @@
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
+
+// tells Rust to import the rocket crate and allow the use of macros defined in the crate
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -8,6 +10,9 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+// creates a new instance of the Rocket application, mounts the index route at the root ("/"), and launches the server
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
 }
+
+
