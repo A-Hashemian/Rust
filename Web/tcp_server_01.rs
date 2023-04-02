312d3dda6bfddaf17c8a91c41265881fe4f9d2ad
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-04-02 11:21:49 -0400

    Update tcp_server_01.rs

--- Web/tcp_server_01.rs
+++ Web/tcp_server_01.rs
@@ -3,5 +3,12 @@ use std::io::{Read, Write};
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-   
+       let bytes_read = match stream.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => n,
+            Err(e) => {
+                println!("Failed to read from socket: {}", e);
+                break;
+            }
+        };
 }
