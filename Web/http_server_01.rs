3b5956caae218f361cdb4b3f0f10dfbf7fc585d3
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-05-01 23:58:57 +0300

    Update http_server_01.rs

--- Web/http_server_01.rs
+++ Web/http_server_01.rs
@@ -35,7 +35,7 @@ let responses = futures::future::join_all(
 
 /*
 this section of code uses the futures::future::join_all function to create a future that sends all the requests asynchronously
-
+the join_all function takes an iterator of futures and returns a future that resolves to a vector of the results of each future
 
 
 */
