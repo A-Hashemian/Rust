8b456f28a5dbf63b51471a80227514b7f23f0318
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-05-01 23:37:38 +0300

    Update http_server_01.rs

--- Web/http_server_01.rs
+++ Web/http_server_01.rs
@@ -1,2 +1,9 @@
 
 use reqwest::Client;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+/*
+This line is a Rust macro that sets up a tokio runtime, which allows us to run asynchronous code
+The #[tokio::main] attribute on the main function tells Rust to use tokio as the runtime for this program
+
+*/
