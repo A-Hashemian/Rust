3b4f71351389925b04f8f39a522d9173e4f8ee07
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-05-04 23:41:22 +0300

    Update db_conn_00.rs

--- Web/db_conn_00.rs
+++ Web/db_conn_00.rs
@@ -5,5 +5,14 @@ fn main() -> Result<()> {
     // Open a database connection
     let conn = Connection::open("example.db")?;
   
+      // Create a table called "person"
+    conn.execute(
+        "CREATE TABLE person (
+                  id              INTEGER PRIMARY KEY,
+                  name            TEXT NOT NULL,
+                  age             INTEGER NOT NULL
+                  )",
+        [],
+    )?;
   
   }
