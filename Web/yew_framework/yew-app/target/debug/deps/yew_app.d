bcf945878d7283d4a71dbdf890cc43ff3390a3c4
A-Hashemian <amin@hashemian.tech>
2023-06-24 17:07:13 +0300

    update

---
+++ Web/yew_framework/yew-app/target/debug/deps/yew_app.d
@@ -0,0 +1,5 @@
+F:\New Amen\Revolutation2023\Rust\Rust\Web\yew_framework\yew-app\target\debug\deps\yew_app.exe: src\main.rs
+
+F:\New Amen\Revolutation2023\Rust\Rust\Web\yew_framework\yew-app\target\debug\deps\yew_app.d: src\main.rs
+
+src\main.rs:
---
+++ Web/yew_framework/yew-app/target/debug/yew-app.d
@@ -0,0 +1,1 @@
+F:\New\ Amen\Revolutation2023\Rust\Rust\Web\yew_framework\yew-app\target\debug\yew-app.exe: F:\New\ Amen\Revolutation2023\Rust\Rust\Web\yew_framework\yew-app\src\main.rs
