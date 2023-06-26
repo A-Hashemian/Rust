37510cca64b6e1a2e6beb657d3aa139ee51a3416
A-Hashemian <amin@hashemian.tech>
2023-06-26 23:20:33 +0300

    up_01

--- Web/yew_framework/pro_02/pro-02/src/model.rs
+++ Web/yew_framework/pro_02/pro-02/src/model.rs
@@ -3,3 +3,12 @@
 use yew::{html, Component, ComponentLink, Html, ShouldRender};
 use yew::format::Json;
 use yew::services::storage::{Area, StorageService};
+
+
+
+mod item;
+mod modal;
+mod input;
+
+use crate::item::Item;
+use crate::modal::Modal;
