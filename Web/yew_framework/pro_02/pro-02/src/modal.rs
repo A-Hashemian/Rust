470664108582c5c6e05c427797228c3abe8dd7ff
A-Hashemian <amin@hashemian.tech>
2023-07-06 22:37:20 +0300

    up_18

--- Web/yew_framework/pro_02/pro-02/src/modal.rs
+++ Web/yew_framework/pro_02/pro-02/src/modal.rs
@@ -4,3 +4,11 @@ use yew::{html, Component, ComponentLink, Html, ShouldRender, Callback, Properti
 use yew::services::{
     ConsoleService
   };
+
+  #[derive(Properties, Clone)]
+pub struct ModalProperties {
+  pub item: Item,
+  pub visible: bool,
+  pub on_close: Callback<bool>,
+  pub on_save: Callback<Item>
+}
